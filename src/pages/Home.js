a1790dc8d1f4757d3d078543e955b96a4efec68e
Vinícius Vieira <florianovinicius09@gmail.com>
2021-03-19 17:20:31 -0300

    Inicio do desenvolvimento usando react navigation

---
+++ src/pages/Home.js
@@ -0,0 +1,42 @@
+//import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Botao from '../components/Botao';
+
+
+//Página inicial do aplicativo
+export default function Home( {navigation} ) {
+  return (
+   <View style={styles.container}>
+      <Text style={styles.tituloInicial}>Swim Tests</Text>
+
+      <Text style={styles.textoInicial}>Bem vindo ao Swim Tests! 
+      É um prazer ajudar atletas e professores a alcançar seus objetivos.
+      Clique no botão abaixo para começar.</Text>
+
+      <Botao value="Começar" onPress= {() => navigation.navigate('Menu')}></Botao>
+    </View>
+    
+  );
+}
+
+//Configuração de estilos de componentes
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+   
+  },
+  tituloInicial: {
+    marginTop: 100,
+    marginBottom: 93,
+    fontSize: 36,
+  },
+  textoInicial: {
+    fontSize: 25, 
+    textAlign: 'center',
+    marginBottom: 120
+
+  }
+});
---
+++ src/pages/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View } from 'react-native';
+import Botao from '../components/Botao';
+
+//Página inicial do aplicativo
+export default function App() {
+  return (
+    
+    <View>
+        <Botao value="Ta mec"> </Botao>
+    </View>
+  );
+}
